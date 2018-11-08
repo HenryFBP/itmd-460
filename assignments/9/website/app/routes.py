7611716ce9d67f5fc56abc656c7e4f4c7ab8f112
HenryFBP <HenryFBP@gmail.com>
2018-11-07 22:16:46 -0600

    begin working on audio section

--- assignments/9/website/app/routes.py
+++ assignments/9/website/app/routes.py
@@ -4,10 +4,21 @@ from app import app
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 
-@app.route('/intro')
-def intro():
-    return render_template('intro.html')
+@app.route('/audio')
+def audio():
+    return render_template('audio.html')
+
+
+@app.route('/video')
+def video():
+    return render_template('video.html')
+
+
+@app.route('/images')
+def images():
+    return render_template('images.html')
--- assignments/9/website/app/start_app.py
+++ assignments/9/website/app/start_app.py
@@ -1,4 +1,6 @@
 from app import app
 
+from app.routes import index, audio, images, video
+
 if __name__ == '__main__':
     app.run()
