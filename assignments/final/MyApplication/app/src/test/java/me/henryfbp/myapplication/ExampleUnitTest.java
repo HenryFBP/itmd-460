8f4e3f3288ce4aca83e33e71bcdc5b9c7e6a6f88
Henry Post <HenryFBP@gmail.com>
2018-11-12 01:54:24 -0600

    work on code overview

--- assignments/final/MyApplication/app/src/test/java/me/henryfbp/myapplication/ExampleUnitTest.java
+++ assignments/final/MyApplication/app/src/test/java/me/henryfbp/myapplication/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package me.henryfbp.myapplication;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,23 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+
+        System.out.println(stuff(20));
+    }
+
+    // Example of simple Java syntax. Used in 'Java crash course'.
+    public java.lang.Integer stuff(Integer argument) {
+
+        if (argument >= 20) {
+            return (argument + 200);
+        }
+
+        String msg = "Hello there!";
+
+        if (msg.contains("the")) {
+            return 20;
+        } else {
+            return -1;
+        }
     }
 }
